ba71089ad2acd0290a03259039f14c73a72c655e
karkikusal <karkikusal16@gmail.com>
2023-02-13 19:23:12 +0545

    first

---
+++ kusal.java
@@ -0,0 +1,14 @@
+public class kusal{
+    public static void main(String[] args){
+    System.out.print("Name of the organization: Rambo travel" );
+    
+     System.out.print("Address:");
+     System.out.print("----State:Madhesh 02");
+     System.out.print("----Zip Code:7896");
+     System.out.print("----Country:  Nepal");
+
+     System.out.print("Contact Information:");
+     System.out.print("----Mobile Number:9874568");
+     System.out.print("----Email:karkikusal16@gmail.com");
+    }
+}
